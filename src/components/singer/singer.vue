9ceb9ae3053e6ed3116825bc62254f46f200910e
unknown <yefei-yuefei@163.com>
2018-06-15 18:18:53 +0800

    first commit

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="singer" ref="singer">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
